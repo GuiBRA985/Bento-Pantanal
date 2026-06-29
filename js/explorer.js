5875c3906b0007547586523327fc44f5ca41a2e2
GuiBRA985 <gui@bento.host>
2026-06-29 18:22:12 -0400

    Update explorer.js

--- js/explorer.js
+++ js/explorer.js
@@ -1,17 +1,23 @@
-const map = L.map('map').setView([-16.560, -56.710], 9);
+const map = L.map("map").setView([-16.56, -56.71], 9);
 
 L.tileLayer(
-'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-{
-maxZoom:19,
-attribution:'© OpenStreetMap'
-}
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+        maxZoom: 19,
+        attribution: "© OpenStreetMap"
+    }
 ).addTo(map);
 
-L.marker([-16.560,-56.710])
-.addTo(map)
-.bindPopup("<b>Poconé</b><br>Início da Transpantaneira.");
+locais.forEach(local => {
 
-L.marker([-16.370,-56.930])
-.addTo(map)
-.bindPopup("<b>Porto Jofre</b>");
+    L.marker([local.lat, local.lng])
+
+        .addTo(map)
+
+        .bindPopup(`
+            <strong>${local.nome}</strong><br>
+            Km ${local.km}<br><br>
+            ${local.descricao}
+        `);
+
+});
