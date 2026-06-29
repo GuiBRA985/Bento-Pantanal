b690614fa4538f077bd6efb45eed691717bc9948
GuiBRA985 <gui@bento.host>
2026-06-29 18:20:11 -0400

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,43 @@
+const locais = [
+
+    {
+        id: 1,
+        tipo: "cidade",
+        nome: "Poconé",
+        km: 0,
+        lat: -16.2567,
+        lng: -56.6228,
+        descricao: "Portal de entrada da Transpantaneira."
+    },
+
+    {
+        id: 2,
+        tipo: "hotel",
+        nome: "Pousada Piuval",
+        km: 10,
+        lat: -16.592,
+        lng: -56.741,
+        descricao: "Excelente para observação de aves."
+    },
+
+    {
+        id: 3,
+        tipo: "hotel",
+        nome: "Pousada Rio Claro",
+        km: 42,
+        lat: -16.705,
+        lng: -56.846,
+        descricao: "Região tradicional da Transpantaneira."
+    },
+
+    {
+        id: 4,
+        tipo: "destino",
+        nome: "Porto Jofre",
+        km: 145,
+        lat: -16.495,
+        lng: -56.995,
+        descricao: "Principal destino para observação de onças."
+    }
+
+];
