8a63bd64e9fbc694d88337c9510a8b311f5b22d6
GuiBRA985 <gui@bento.host>
2026-06-29 18:14:36 -0400

    Create pousadas.js

---
+++ js/data/pousadas.js
@@ -0,0 +1,32 @@
+const pousadas = [
+  {
+    id: 1,
+    nome: "Pousada Piuval",
+    km: 10,
+    lat: -16.592,
+    lng: -56.741,
+    descricao: "Excelente para observação de aves e fauna.",
+    site: "",
+    whatsapp: ""
+  },
+  {
+    id: 2,
+    nome: "Pousada Rio Claro",
+    km: 42,
+    lat: -16.705,
+    lng: -56.846,
+    descricao: "Uma das regiões mais tradicionais da Transpantaneira.",
+    site: "",
+    whatsapp: ""
+  },
+  {
+    id: 3,
+    nome: "Porto Jofre",
+    km: 145,
+    lat: -16.495,
+    lng: -56.995,
+    descricao: "Principal destino para observação de onças.",
+    site: "",
+    whatsapp: ""
+  }
+];
