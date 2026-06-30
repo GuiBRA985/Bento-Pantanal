95c1ac9183e4772e6661f9309ec56ab79822a4c3
GuiBRA985 <gui@bento.host>
2026-06-30 10:07:08 -0400

    Update data.js

--- js/data.js
+++ js/data.js
@@ -21,14 +21,17 @@ const locais = [
     },
 
     {
-        id: 3,
-        tipo: "hotel",
-        nome: "Pousada Rio Claro",
-        km: 42,
-        lat: -16.705,
-        lng: -56.846,
-        descricao: "Região tradicional da Transpantaneira."
-    },
+    id: 3,
+    tipo: "hotel",
+    nome: "Pousada Rio Claro",
+    km: 42,
+    lat: -16.705,
+    lng: -56.846,
+    descricao: "Uma das regiões mais tradicionais da Transpantaneira.",
+    foto: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=1200",
+    site: "https://www.rioclarolodge.com.br",
+    whatsapp: "5565999999999"
+},
 
     {
     id: 4,
