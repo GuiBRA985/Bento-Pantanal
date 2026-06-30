247f10f0201e1e230be2f9935a4e58cfda0c0dfb
GuiBRA985 <gui@bento.host>
2026-06-30 09:23:13 -0400

    Update explorer.js

--- js/explorer.js
+++ js/explorer.js
@@ -3,21 +3,41 @@ const map = L.map("map").setView([-16.56, -56.71], 9);
 L.tileLayer(
     "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
     {
-        maxZoom: 19,
-        attribution: "© OpenStreetMap"
+        maxZoom:19,
+        attribution:"© OpenStreetMap"
     }
 ).addTo(map);
 
-locais.forEach(local => {
+const panel=document.getElementById("info-panel");
 
-    L.marker([local.lat, local.lng])
+const content=document.getElementById("panel-content");
 
-        .addTo(map)
+document.getElementById("close-panel").onclick=()=>{
+    panel.classList.remove("show");
+};
 
-        .bindPopup(`
-            <strong>${local.nome}</strong><br>
-            Km ${local.km}<br><br>
-            ${local.descricao}
-        `);
+locais.forEach(local=>{
+
+    const marker=L.marker([local.lat,local.lng]).addTo(map);
+
+    marker.on("click",()=>{
+
+        content.innerHTML=`
+            <h2>${local.nome}</h2>
+
+            <p><strong>Km:</strong> ${local.km}</p>
+
+            <p>${local.descricao}</p>
+
+            <br>
+
+            <button style="padding:12px 18px;border:none;background:#173A2B;color:white;border-radius:8px;">
+                Como chegar
+            </button>
+        `;
+
+        panel.classList.add("show");
+
+    });
 
 });
