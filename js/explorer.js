adb54d0f27dc1aa8ea38cb130d1a76a6f680c17c
GuiBRA985 <gui@bento.host>
2026-06-30 09:38:46 -0400

    Update explorer.js

--- js/explorer.js
+++ js/explorer.js
@@ -15,6 +15,30 @@ const content=document.getElementById("panel-content");
 document.getElementById("close-panel").onclick=()=>{
     panel.classList.remove("show");
 };
+const icones = {
+
+    cidade: L.icon({
+        iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png",
+        shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+        iconSize: [25,41],
+        iconAnchor: [12,41]
+    }),
+
+    hotel: L.icon({
+        iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png",
+        shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+        iconSize: [25,41],
+        iconAnchor: [12,41]
+    }),
+
+    destino: L.icon({
+        iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
+        shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+        iconSize: [25,41],
+        iconAnchor: [12,41]
+    })
+
+};
 
 locais.forEach(local=>{
 
