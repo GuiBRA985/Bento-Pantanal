931043116e069ca1c17b526461873dd7644bdfbe
GuiBRA985 <gui@bento.host>
2026-07-01 09:05:34 -0400

    Create destinos.js

---
+++ js/data/destinos.js
@@ -0,0 +1,13 @@
+const destinos = [
+
+{
+    id:4,
+    tipo:"destino",
+    nome:"Porto Jofre",
+    km:145,
+    lat:-16.4958,
+    lng:-56.3725,
+    descricao:"Principal destino para observação de onças."
+}
+
+];
