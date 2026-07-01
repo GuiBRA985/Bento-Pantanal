de15b21c130e2c07c60a3d1dcf8afa13e0425a20
GuiBRA985 <gui@bento.host>
2026-07-01 09:05:07 -0400

    Update pousadas.js

--- js/data/pousadas.js
+++ js/data/pousadas.js
@@ -1,32 +1,26 @@
+
 const pousadas = [
-  {
-    id: 1,
-    nome: "Pousada Piuval",
-    km: 10,
-    lat: -16.592,
-    lng: -56.741,
-    descricao: "Excelente para observação de aves e fauna.",
-    site: "",
-    whatsapp: ""
-  },
-  {
-    id: 2,
-    nome: "Pousada Rio Claro",
-    km: 42,
-    lat: -16.705,
-    lng: -56.846,
-    descricao: "Uma das regiões mais tradicionais da Transpantaneira.",
-    site: "",
-    whatsapp: ""
-  },
-  {
-    id: 3,
-    nome: "Porto Jofre",
-    km: 145,
-    lat: -16.495,
-    lng: -56.995,
-    descricao: "Principal destino para observação de onças.",
-    site: "",
-    whatsapp: ""
-  }
+
+{
+    id:2,
+    tipo:"hotel",
+    nome:"Pousada Piuval",
+    km:10,
+    lat:-16.592,
+    lng:-56.741,
+    descricao:"Excelente para observação de aves."
+},
+
+{
+    id:3,
+    tipo:"hotel",
+    nome:"Pousada Rio Claro",
+    km:42,
+    lat:-16.705,
+    lng:-56.846,
+    descricao:"Uma das regiões mais tradicionais da Transpantaneira.",
+    foto:"https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=1200",
+    site:"https://www.rioclarolodge.com.br"
+}
+
 ];
